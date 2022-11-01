8a4f0f1b5a8ad54689487f32a7664ce9cbdddadf
Ольга Вишневская <vishnievska@knu.ua>
2022-11-01 19:46:27 +0200

    Добавьте файлы проекта.

---
+++ Json-HomeTask/Program.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+
+public class Book
+{
+    [JsonIgnore]
+    public int? PublishingHouseId { get; private set; }
+    [JsonPropertyName("Title")]
+    public string Name { get; private set; }
+    public PublishingHouse? PublishingHouse { get; private set; }
+}
+
+public record class PublishingHouse (int Id, string Name, string Adress);
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        const string Path = "./books.json";
+
+        var jsonContent = File.ReadAllText(Path);
+
+        var books = JsonSerializer.Deserialize<List<Book>>(jsonContent, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true,
+        });
+
+        if (books != null)
+        {
+            foreach (var book in books)
+            {
+                Console.WriteLine($"Код видавництва: {book.PublishingHouseId}; Книга: {book.Name}.");
+                Console.WriteLine($"Видавництво: { book.PublishingHouse?.Id}, { book.PublishingHouse.Name}, { book.PublishingHouse.Adress}.");
+                Console.WriteLine();
+            }
+        }
+
+        Console.ReadKey();
+    }
+}
